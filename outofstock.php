a7527e02f5723660c0d78ac4e106a8ef6d926af8
Anshul-Sharma12 <anshulsharma052005@gmail.com>
2024-09-16 16:43:26 +0530

    read more text changed to out of stock

---
+++ outofstock.php
@@ -0,0 +1,15 @@
+<?php
+// Change the "Read More" text to "Out of Stock" for out-of-stock products on product listings
+add_filter('woocommerce_loop_add_to_cart_link', 'custom_out_of_stock_text', 10, 2);
+function custom_out_of_stock_text($link, $product) {
+    // Check if the product is out of stock
+    if (!$product->is_in_stock()) {
+        // Change the "Read More" text to "Out of Stock"
+        $link = '<span class="button out-of-stock">Out of Stock</span>';
+    }
+    return $link;
+}
+?>
+
+
+input this code before the add_footer action
